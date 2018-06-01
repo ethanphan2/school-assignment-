4552ce7cb4e2b57949473addbbc71d0f7d79378f
jotaeo <37418589+jotaeo@users.noreply.github.com>
2018-06-01 11:30:24 -0400

    Update turn based combat.py

--- turn based combat.py
+++ turn based combat.py
@@ -5,6 +5,7 @@ alienhp = int (1000)
 playeralive = (True)
 alienalive = (True)
 
+
 print (name , "encounters a weird being")
 print (" does",name,"...")
 print ("A. fight")
@@ -20,3 +21,11 @@ if answer == "A" or "a":
     print ("A. heavy attack")
     print ("B. light attack")
     print ("C.gun attack")
+    answer = input ("answer: ")
+    if answer == "A" or "a":
+      print (name , "went for a heavy attack!!")
+      dmg = random.randint(70,100)
+      alienhp = dmg - alienhp
+
+      print ("you did" , dmg ,"damage to the alien!! it now has " , alienhp)
+
